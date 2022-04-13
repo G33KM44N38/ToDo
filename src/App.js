dd966fea63c89e59137794b5ecc4992fa0e15de8
Kylian <kylianmayanga@gmail.com>
2022-04-13 14:23:20 +0200

    The home and the button works well

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Active from "./components/Active";
+import All from "./components/All";
+import Completed from "./components/Completed";
 
 function App() {
+
+  const [Tab, setTab] = useState("All");
+
   return (
     <div className="App">
+      <h1>#todo</h1>
+      <div>
+        <button onClick={() => setTab("All")}>All</button>
+        <button onClick={() => setTab("Active")}>Active</button>
+        <button onClick={() => setTab("Completed")}>Completed</button>
+      </div>
+      <div>
+        {Tab === "All" ? <All/> : null}
+        {Tab === "Active" ? <Active/> : null}
+        {Tab === "Completed" ? <Completed/> : null}
+      </div>
     </div>
   );
 }
