b35052654804639a0ce342be7115d2e41613c870
Kylian <kylianmayanga@gmail.com>
2022-04-15 11:04:15 +0200

    I changed the way to have an id

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import React, { useState } from "react";
 import Active from "./components/Active";
 import All from "./components/All";
 import Completed from "./components/Completed";
-import { v4 as uuidv4 } from 'uuid';
-
-
-
+import uniqueid from "./function/getNewId";
 
 function App() {
 
   const [Tab, setTab] = useState("All")
   const [NewTaskName, setNewTaskName] = useState("")
   const [ToDoList, setToDoList] = useState([
-    {id: uuidv4(), name: "Shop some coffee", done: false },
-    {id: uuidv4(), name: "Buy milk", done: true }
+    {id: 1, name: "Shop some coffee", done: false },
+    {id: 2, name: "Buy milk", done: true }
   ])
+
   const addNewTask = (e) => {
 
     e.preventDefault()
 
     if (NewTaskName !== '')
     {
-      setToDoList([...ToDoList, {id: uuidv4(), name: NewTaskName, done: false}])
+      setToDoList([...ToDoList, {id: uniqueid(), name: NewTaskName, done: false}])
       setNewTaskName("")
     }
   }
